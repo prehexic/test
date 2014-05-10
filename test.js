12cdde2fd68f5839c555b88f5dcafd5f166d97c9
prehexic <allan@allanbeck.co.uk>
2014-05-10 15:42:41 +0100

    test

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 var http = require('http');
+var host = process.env.VCAP_APP_HOST || process.env.OPENSHIFT_NODEJS_IP || 'localhost';
+var port = process.env.VCAP_APP_PORT || process.env.OPENSHIFT_NODEJS_PORT || '3000';
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
-});
-server.listen(8080);
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, host);
+console.log('Server running on: ' + host + ":"+ port);
